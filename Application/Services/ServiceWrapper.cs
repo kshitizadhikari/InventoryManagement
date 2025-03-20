d2a62ec237835dea3c4271aa664b2daa182ed114
kshitizadhikari <adhikarikshitiz12@gmail.com>
2025-03-20 15:24:44 +0545

    Add ServiceWrapper

---
+++ Application/Services/ServiceWrapper.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Interfaces.Services;
+
+namespace Application.Services;
+public class ServiceWrapper(ICategoryService categoryService) : IServiceWrapper
+{
+    public ICategoryService _categoryService = categoryService;
+
+    public ICategoryService CategoryService => _categoryService;
+}
+
