c582eda5f9fbe1ce762b4b59edd0599fbb0d3243
kshitizadhikari <adhikarikshitiz12@gmail.com>
2025-03-20 15:24:34 +0545

    Fix CreateAsync in CategoryService

--- Application/Services/CategoryService.cs
+++ Application/Services/CategoryService.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Mappers;
+using Domain.Entities;
 using Domain.Entities.DTOs;
 using Domain.Interfaces.Repositories;
 using Domain.Interfaces.Services;
@@ -8,14 +9,27 @@ public class CategoryService(IRepositoryWrapper repos) : ICategoryService
 {
     private readonly IRepositoryWrapper _repos = repos;
 
-    public async Task<CategoryDTO> CreateAsync(CategoryDTO dto)
+    public async Task<(CategoryDTO?, ErrorResponse?)> CreateAsync(CategoryDTO dto)
     {
+        if (dto == null)
+        {
+            return (null, new ErrorResponse
+            {
+                ErrorCode = 400,
+                Title = "Creation Error",
+                Message = "Category data cannot be null"
+            });
+        }
         var entity = dto.MapToEntity();
-        return ((await _repos.CategoryRepository.CreateAsync(entity)).MapToDTO());
+
+        var res = await _repos.CategoryRepository.CreateAsync(entity);
+        var resDto = res.MapToDTO();
+
+        return (resDto, null);
     }
 
     public async Task<List<CategoryDTO>> GetAllAsync()
     {
-        return ((await _repos.CategoryRepository.GetAllAsync()).MapToDTO());
+        return (await _repos.CategoryRepository.GetAllAsync()).MapToDTO();
     }
 }
