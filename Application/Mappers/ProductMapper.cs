ced70cfdc9d03e85ce7f03c0c1136032599b8d57
kshitiz <adhikarikshitiz12@gmail.com>
2025-03-24 23:38:54 +0545

    Create ProductMapper

---
+++ Application/Mappers/ProductMapper.cs
@@ -0,0 +1,42 @@
+﻿using Domain.Entities;
+using Domain.Entities.DTOs;
+
+namespace Application.Mappers;
+public static class ProductMapper
+{
+    public static Product MapToEntity(this ProductDTO dto)
+    {
+        return new Product
+        {
+            Id = string.IsNullOrEmpty(dto.Id) ? Guid.NewGuid() : Guid.Parse(dto.Id),
+            Name = dto.Name,
+            Description = dto.Description,
+            Price = dto.Price,
+            CategoryId = Guid.Parse(dto.CategoryId),
+            SubCategoryId = Guid.Parse(dto.SubCategoryId)
+        };
+    }
+
+    public static List<Product> MapToEntity(this IList<ProductDTO> dtos)
+    {
+        return dtos.Select(x => x.MapToEntity()).ToList();
+    }
+
+    public static ProductDTO MapToDTO(this Product entity)
+    {
+        return new ProductDTO
+        {
+            Id = entity.Id.ToString(),
+            Name = entity.Name,
+            Description = entity.Description,
+            Price = entity.Price,
+            CategoryId = entity.CategoryId.ToString(),
+            SubCategoryId = entity.SubCategoryId.ToString()
+        };
+    }
+
+    public static List<ProductDTO> MapToDTO(this IList<Product> entities)
+    {
+        return entities.Select(x => x.MapToDTO()).ToList();
+    }
+}
