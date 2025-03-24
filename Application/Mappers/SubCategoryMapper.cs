eb89f6b6b7c2cf2cec28e82381d3e561b4385905
kshitiz <adhikarikshitiz12@gmail.com>
2025-03-24 15:57:13 +0545

    Add SubCategoryMapper

---
+++ Application/Mappers/SubCategoryMapper.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Entities;
+using Domain.Entities.DTOs;
+
+namespace Application.Mappers;
+public static class SubSubCategoryMapper
+{
+    public static SubCategory MapToEntity(this SubCategoryDTO dto)
+    {
+        return new SubCategory
+        {
+            Id = string.IsNullOrEmpty(dto.Id) ? Guid.NewGuid() : Guid.Parse(dto.Id),
+            Name = dto.Name,
+            CategoryId = Guid.Parse(dto.CategoryId)
+        };
+    }
+
+    public static List<SubCategory> MapToEntity(this IList<SubCategoryDTO> dtos)
+    {
+        return dtos.Select(x => x.MapToEntity()).ToList();
+    }
+
+    public static SubCategoryDTO MapToDTO(this SubCategory entity)
+    {
+        return new SubCategoryDTO
+        {
+            Id = entity.Id.ToString(),
+            Name = entity.Name,
+            CategoryId = entity.CategoryId.ToString(),
+        };
+    }
+
+    public static List<SubCategoryDTO> MapToDTO(this IList<SubCategory> entities)
+    {
+        return entities.Select(x => x.MapToDTO()).ToList();
+    }
+}
